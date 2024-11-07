1c698e200193ba5912381b5646514f5c7fab90e3
MichaelManullang <82314256+MichaelManullang@users.noreply.github.com>
2024-11-07 14:58:39 +0700

    sim_wbs16 (#11)
    * sim_wbs16
    
    * Parameterisasi top_interface dan testbench
    
    * Revisi top_interface & testbench
    
    * Revisi top_interface & testbench (2)

---
+++ sim/sim_wb16/packages.svh
@@ -0,0 +1,11 @@
+package packages;
+    import uvm_pkg::*;
+
+    // `include "sequence.svh"
+    // `include "driver.svh"
+    // `include "monitor.svh"
+    // `include "agent.svh"
+    // `include "scoreboard.svh"
+    // `include "environment.svh"
+    `include "test.svh"
+endpackage
