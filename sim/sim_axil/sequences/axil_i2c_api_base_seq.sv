139fc1a301bc1c24d96cf16f8d6fa368611fdfdb
zeroleaked <zirlysukarno@gmail.com>
2024-11-08 12:40:26 +0700

    clean up i2c base seq

--- sim/sim_axil/sequences/axil_i2c_api_base_seq.sv
+++ sim/sim_axil/sequences/axil_i2c_api_base_seq.sv
@@ -4,24 +4,17 @@
 class i2c_api_base_seq extends uvm_sequence #(i2c_seq_item);
     `uvm_object_utils(i2c_api_base_seq)
 
-	i2c_seq_item trans;
-
     function new(string name = "i2c_api_base_seq");
         super.new(name);
-        trans = i2c_seq_item::type_id::create("req");
     endfunction
 
     task body();
         req = i2c_seq_item::type_id::create("req");
         start_item(req);
-		req.randomize() with {
-			req.address == trans.address;
-		};
+		req.randomize();
         finish_item(req);
 		get_response(rsp);
 		req.print();
-
-		// get_response(rsp);
     endtask
 
 endclass
