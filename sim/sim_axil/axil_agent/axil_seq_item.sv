9e89d8b87c7b55f41727fe334604c8895b87b420
zeroleaked <zirlysukarno@gmail.com>
2024-11-09 20:54:03 +0700

    unused constraint

--- sim/sim_axil/axil_agent/axil_seq_item.sv
+++ sim/sim_axil/axil_agent/axil_seq_item.sv
@@ -26,40 +26,36 @@
 `define AXIL_SEQ_ITEM
 
 class axil_seq_item extends uvm_sequence_item;
-    rand bit [3:0]  addr;
-    rand bit [31:0] data;
-    rand bit [3:0]  strb;
-    rand bit        read;
+	rand bit [3:0]  addr;
+	rand bit [31:0] data;
+	rand bit [3:0]  strb;
+	rand bit        read;
 
 	bit [3:0] cfg_address;
 	bit [31:0] cfg_data;
-    
-    `uvm_object_utils_begin(axil_seq_item)
-        `uvm_field_int(addr, UVM_ALL_ON)
-        `uvm_field_int(data, UVM_ALL_ON)
-        `uvm_field_int(strb, UVM_ALL_ON)
-        `uvm_field_int(read, UVM_ALL_ON)
-    `uvm_object_utils_end
-
-    constraint seq_cfg_address_c {
-        addr == cfg_address;  // 7-bit address range
-    }
-
-    constraint seq_cfg_data_c {
-        data == cfg_data;  // constrain all bytes
-    }
-
-    constraint seq_cfg_data_0_c {
-        data[15:8] == cfg_data[15:8];  // constrain 2nd byte
-    }
-
-    constraint addr_c {
-        addr inside {[0:3]};
-    }
 	
-    function new(string name = "axil_seq_item");
-        super.new(name);
-    endfunction
+	`uvm_object_utils_begin(axil_seq_item)
+		`uvm_field_int(addr, UVM_ALL_ON)
+		`uvm_field_int(data, UVM_ALL_ON)
+		`uvm_field_int(strb, UVM_ALL_ON)
+		`uvm_field_int(read, UVM_ALL_ON)
+	`uvm_object_utils_end
+
+	constraint seq_cfg_address_c {
+		addr == cfg_address;  // 7-bit address range
+	}
+
+	constraint seq_cfg_data_c {
+		data == cfg_data;  // constrain all bytes
+	}
+
+	constraint seq_cfg_data_0_c {
+		data[15:8] == cfg_data[15:8];  // constrain 2nd byte
+	}
+	
+	function new(string name = "axil_seq_item");
+		super.new(name);
+	endfunction
 endclass
 
 `endif
